cc0c3e97d1b48c20d4a56becf9983941581f3919
Your Name <elizabeth.ramos101@hotmail.com>
2023-01-09 13:58:19 +0000

    Auth0

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {Auth0Provider} from '@auth0/auth0-react';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -7,7 +8,13 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+  <Auth0Provider
+    domain='dev-hrdt0ak2uvx8sjvy.us.auth0.com'
+    clientId='wrZOcYjqtIfTOv8JPtlaYlIpHmm0BcCO'
+    redirectUri= {window.location.origin}
+    >
+      <App />
+  </Auth0Provider>
   </React.StrictMode>
 );
 
